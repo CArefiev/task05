5f588f7aecccc3b01aa8d627296f90f3337807ae
CArefiev <cristina.arefiev@endava.com>
2020-10-29 01:12:32 +0200

    Changes to main class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("Hello!");
-=======
-        System.out.println("Hello World!");
->>>>>>> b3bf2a607af1be698a47a5eeb7dbf1851d128037
+
     }
 }
