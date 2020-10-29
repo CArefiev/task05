6ec6df2ab4d5eeeb0da34140fb8428fad81ddca1
CArefiev <cristina.arefiev@endava.com>
2020-10-29 02:14:33 +0200

    One new method to main class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
+
+    public static Double factorial(Integer a) {
+
+        double fact = 1;
+        for (int i = 1; i <= a; i++) {
+
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+
     public static void main(String[] args) {
 
-        System.out.println("Hi!!");
+        System.out.println(factorial(5));
 
     }
 }
