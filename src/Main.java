46bc4383c50e8691dd4c31a5857f6a12a8de194a
CArefiev <cristina.arefiev@endava.com>
2020-10-29 02:05:15 +0200

    Added methods to main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+
+    static void sum (int a, int b) {
+
+        System.out.println("The summ of a and b is: " + a + b);
+    }
+
+    static void prod (int a, int b) {
+
+        System.out.println("The product of a and b is: " + a * b);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    sum(10,30);
+    prod(10,30);
+
     }
 }
