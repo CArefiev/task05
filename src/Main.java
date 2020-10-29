4e771e40f0dd5510e22b5c1c1b1141338930b036
CArefiev <cristina.arefiev@endava.com>
2020-10-29 22:02:41 +0200

    Main method

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,6 @@
 public class Main {
 
-<<<<<<< HEAD
-    static void sum (int a, int b) {
-
-        System.out.println("The summ of a and b is: " + a + b);
-    }
-
-    static void prod (int a, int b) {
-
-        System.out.println("The product of a and b is: " + a * b);
-    }
-
-    public static Double factorial(Integer a) {
-
-        double fact = 1;
-        for (int i = 1; i <= a; i++) {
-
-            fact = fact * i;
-        }
-        return fact;
-    }
-
-
     public static void main(String[] args) {
-
-        System.out.println(factorial(5));
-
-
+        
     }
 }
